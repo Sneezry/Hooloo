fa25300f2b0e322aed2658ac9a4fbfde535aef35
Sneezry <lizhe@lizhe.org>
2014-02-11 12:56:49 +0800

    del short url

--- javascripts/getmd.js
+++ javascripts/getmd.js
@@ -105,11 +105,10 @@ function loadXMLDoc(url){
 						encoded = true;
 					};
 					var converter = new Showdown.converter();
-					content.innerHTML = '<div style="padding: 20px 20px 20px 40px;"><div id="back_home"><a href="/" onclick="home();return false;">'+sitetitle+'</a><span>&nbsp;›&nbsp;</span></div><div id="post_title">' + decodeUtf8(path.substr(1).split('/')[path.substr(1).split('/').length-1].replace(/_/g, ' ')) + (encoded?Base64.decode('PHN1cCBzdHlsZT0iZm9udC1zaXplOjAuNWVtO3ZlcnRpY2FsLWFsaWduOiBzdXBlcjsiIHRpdGxlPSLmraTmlofnq6Dlt7Looqvph43mlrDnvJbnoIHku6XourLpgb/lrqHmn6UiPuKYmuiiq+e8lueggeeahOWGheWuuTwvc3VwPg=='):'') + '<div id="shorturl">Checking short URL for this post...</div></div>' + converter.makeHtml(blog_text) + '<div class="date"><span>S</span>Posted at ' + pdate + '</div></div>';
+					content.innerHTML = '<div style="padding: 20px 20px 20px 40px;"><div id="back_home"><a href="/" onclick="home();return false;">'+sitetitle+'</a><span>&nbsp;›&nbsp;</span></div><div id="post_title">' + decodeUtf8(path.substr(1).split('/')[path.substr(1).split('/').length-1].replace(/_/g, ' ')) + (encoded?Base64.decode('PHN1cCBzdHlsZT0iZm9udC1zaXplOjAuNWVtO3ZlcnRpY2FsLWFsaWduOiBzdXBlcjsiIHRpdGxlPSLmraTmlofnq6Dlt7Looqvph43mlrDnvJbnoIHku6XourLpgb/lrqHmn6UiPuKYmuiiq+e8lueggeeahOWGheWuuTwvc3VwPg=='):'') + '</div>' + converter.makeHtml(blog_text) + '<div class="date"><span>S</span>Posted at ' + pdate + '</div></div>';
 					if(dis){
 						dis.style.display = 'block';
 					}
-					setTimeout(shorturl,200);
 				}
 				else if(xmlhttp.status==404) {
 					document.title = 'Not Found - '+sitetitle;
@@ -123,18 +122,6 @@ function loadXMLDoc(url){
 		}
 		xmlhttp.open("GET",url,true);
 		xmlhttp.send(null);
-	}
-}
-
-function shorturl(url){
-	if(url && document.getElementById('shorturl')){
-		document.getElementById('shorturl').innerHTML = 'Short URL for this post: <span>'+url+'</span>';
-	}
-	else{
-		var hash = encodeURIComponent(decodeUtf8(location.hash));
-		var el = document.createElement('script');
-		el.src = 'http://sneezryworks.sinaapp.com/blogshorturl.php?url='+encodeURIComponent('http://sneezry.com/'+hash)+'&callback=shorturl';
-		document.getElementsByTagName('head')[0].appendChild(el);
 	}
 }
 
